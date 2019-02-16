b26b3b969b557d609a2a7e2ad52f8e0031515751
Andre Mesquita <avmesquita@gmail.com>
2019-02-16 21:16:47 -0200

    First commit

---
+++ avmJiraIntegration/avmJiraIntegration.App/JiraIssue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avmJiraIntegration.App
+{
+    public class JiraIssue
+    {
+        public JiraIssue()
+        {
+
+        }
+
+        public JiraIssue(string project, string key, string description, string status, string summary, string reporter, string assignee, 
+                         string idPriority, string namePriority, DateTime? duedate, DateTime? created, DateTime? updated, DateTime? resolution)
+        {
+            this.Project = project;
+            this.Key = key;
+            this.Description = description;
+            this.Status = status;
+            this.Reporter = reporter;
+            this.Assignee = assignee;
+            this.Summary = summary;
+            this.Priority = namePriority + "(" + idPriority + ")";
+            this.DueDate = duedate;
+            this.Created = created;
+            this.Updated = updated;
+            this.Resolution = resolution;
+        }
+
+        public string Project { get; set; }
+        public string Key { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Reporter { get; set; }
+        public string Assignee { get; set; }
+        public string Summary { get; set; }
+        public string Priority { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Resolution { get; set; }
+    }
+}
---
+++ avmJiraIntegration/avmJiraIntegration.App/Program.cs
@@ -0,0 +1,74 @@
+﻿using Atlassian.Jira;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace avmJiraIntegration.App
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Integration("username","password","project-name"));
+		}
+
+		static async Task<string> Integration(string username, string password, string project)
+		{
+			var services = new ServiceLocator();
+			var credentials = new Atlassian.Jira.JiraCredentials(username, password);
+			var cache = new JiraCache();
+			
+			var jira = new Jira(services, credentials, cache);			
+			jira.Issues.MaxIssuesPerRequest = 5000;
+
+			/*
+             Status
+             0 - Open
+             1 - In Progress
+             2 - Reopened
+             3 - Resolved
+             4 - Closed
+             5 - Building
+             6 - Build Broken
+             7 - To Do
+             8 - Done
+             9 - Backlog
+             10 - Selected for Development
+             */
+
+			IEnumerable<IssueStatus> listaStatus = await jira.Statuses.GetStatusesAsync();
+			
+
+			var issues = jira.Issues;
+			var projetos = jira.Projects;
+			Console.WriteLine("===============================================");
+			Console.WriteLine("Looking for Project Issues...");
+		    Console.WriteLine("===============================================");
+			foreach (var item in issues.Queryable)
+			{
+				if (item.Project == project && 
+					(item.Status.Id == "0" || item.Status.Id == "1" || item.Status.Id == "2" || item.Status.Id == "7" || item.Status.Id == "9")
+					)
+				{
+					DateTime? resolucao = item.ResolutionDate;
+					resolucao = resolucao == null ? DateTime.Now.AddDays(1) : resolucao;
+
+					DateTime? criacao = item.Created == null ? DateTime.Now.AddDays(1) : item.Created;
+					DateTime? duedate = item.DueDate == null ? DateTime.Now.AddDays(1) : item.DueDate;
+					DateTime? updated = item.Updated == null ? DateTime.Now.AddDays(1) : item.Updated;
+
+					Console.WriteLine("===============================================");
+					Console.WriteLine(item.Project);
+					Console.WriteLine(item.Summary);
+					Console.WriteLine(item.Priority);
+					Console.WriteLine(item.Assignee);
+					Console.WriteLine(item.Reporter);
+					Console.WriteLine(item.Status.Description);
+				}
+			}
+			Console.WriteLine("===============================================");
+
+			return string.Empty;
+		}
+	}
+}
